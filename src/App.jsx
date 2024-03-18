84512cf3acfb251b398cfaf24811a97924f40356
Matic.dev <olaitantoyeeb1@gmail.com>
2024-03-18 22:38:33 +0100

    done with the product card and little design for the filters

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import AllFragance from './Pages/AllFragance'
 
 function App() {
 
   return (
     <>
-    <h1>Final Project</h1>
-      
+    {/* <h1>Final Project</h1> */}
+    <AllFragance/>
     </>
   )
 }
