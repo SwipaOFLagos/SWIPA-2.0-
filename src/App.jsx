f7d4d9a178a42649293fbeded9b1a30f9e728ab7
braitcode <brightjohn489@gmail.com>
2024-03-19 11:02:43 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
+import Hero from './components/Hero'
+
 
 function App() {
 
   return (
     <>
-    <h1>Final Project</h1>
+    <Hero />
+    {/* <h1>Final Project</h1> */}
       
     </>
   )
---
+++ src/components/Hero.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import img1 from "../assets/images/Frame 1171276215.png"
+import img2 from "../assets/images/Frame 1171276214.png"
+import img3 from "../assets/images/Frame 1171276219.png"
+import img4 from "../assets/images/Frame 1171276220.png"
+
+function Hero() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        
+        <img src={img1} text="First slide" width="100%"/>
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={img2} text="Second slide" width="100%"/>
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={img3} text="Third slide" width="100%"/>
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={img4} text="Fourth slide" width="100%"/>
+        <Carousel.Caption>
+          <h3>Fourth slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default Hero;
