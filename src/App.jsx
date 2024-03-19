55cfd15b5f3961031f158fdec8ac81c5c382cf42
mildred <mildred.bailon@yahoo.com>
2024-03-19 10:43:09 +0100

    created branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import Register from './assets/icons/Page/Register'
 
 function App() {
 
   return (
     <>
     <h1>Final Project</h1>
-      
+      <Register/>
     </>
   )
 }
