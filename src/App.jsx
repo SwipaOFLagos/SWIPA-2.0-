c2880812c23d379638dc76f8f60c5686ce4af436
Wealth <shuaibganiyat20@gmail.com>
2024-03-19 10:39:50 +0100

    created a branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
 import './App.css'
 import Login from './pages/Login'
+import DetailPage from './components/DetailPage'
 
 function App() {
 
   return (
     <>
     <h1 className='bg-primary'>Final Project</h1>
-      
+
+      <DetailPage/>
     </>
   )
 }
---
+++ src/components/DetailPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../css/DetailPage.css'
+
+const DetailPage = () => {
+  return (
+    <div>
+        <div className="productCont">
+        <div className="productImg ">Image</div>
+        <div className="productInfo">Info</div>
+        </div>
+    </div>
+  )
+}
+
+export default DetailPage
