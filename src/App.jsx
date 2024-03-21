62151390affad33fbe92db5f272ca5ca532a2349
Blard <peteromu76@gmail.com>
2024-03-21 22:17:08 +0100

    Hero refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 
 import './App.css'
-import Hero from './components/Hero'
-import AllFragance from './Pages/AllFragance'
-import Login from './pages/Login'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home  from './pages/Home'
+import AllFragance from './pages/AllFragance'
 
+
+// WARNING: Do Not change anything in this than just adding your pages to the route.
 
 function App() {
 
   return (
     <>
-    <Hero />
-    <AllFragance/>
-    <h1 className='bg-primary'>Final Project</h1>  
+    <Router>
+      <Routes >
+        <Route path='/' element={<Home/>}/>
+        <Route path='/all-fragrances' element={<AllFragance/>}/>
+        {/* ==== Add the route to your page here === */}
+        
+      </Routes>
+    </Router>
     </>
   )
 }
