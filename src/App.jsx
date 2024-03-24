8392a10acc0e4977b370d2547dc08185aa4c88cb
Nuel <kayodeolawunmi117@gmail.com>
2024-03-24 22:06:19 +0100

    All Fragrance Brands

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Login from './pages/Login'
+import BrandComponent from './components/Brand'
 
 function App() {
 
   return (
     <>
     <h1 className='bg-primary'>Final Project</h1>
+      <BrandComponent/>
       
     </>
   )
