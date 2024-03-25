6573572a0df6f8df6af2abf0c136d30ff7090d31
Bunmi tech <eunicebunmi@ymail.com>
2024-03-25 10:36:32 +0100

    project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 
 import './App.css'
 import AllFragance from './Pages/AllFragance'
-import Login from './pages/Login'
+import Login from './Pages/Login'
+import CustomerDetails from './components/CustomerDetails'
+
 
 function App() {
 
   return (
     <>
     {/* <h1>Final Project</h1> */}
-    <AllFragance/>
-    <h1 className='bg-primary'>Final Project</h1>
-      
+    {/* <h1 className='bg-primary'>Final Project</h1> */}
+    {/* <AllFragance/> */}
+    <CustomerDetails/>
     </>
   )
 }
