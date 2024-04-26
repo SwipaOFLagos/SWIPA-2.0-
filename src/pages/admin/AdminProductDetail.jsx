b9404fbbb1b39e23ca3bb1f12d84a3aa2644c78e
Matic.dev <olaitantoyeeb1@gmail.com>
2024-04-26 21:07:36 +0100

    done with the admin product card

--- src/pages/admin/AdminProductDetail.jsx
+++ src/pages/admin/AdminProductDetail.jsx
@@ -2,21 +2,30 @@ import { useState, useEffect } from "react";
 import { useAuth } from "../../contexts/Auth";
 import Jumbotron from "../../components/cards/Jumbotron";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import Menu from "../../components/NavBar";
 import Footer from "../../components/Footer";
+import "../../css/AdminProductDetail.css";
+import AdminProductCard from "../../components/cards/AdminProductCard";
+import { toast } from "react-toastify";
 
 const AdminProductDetail = () => {
   // context
   const { auth, setAuth } = useAuth();
   const { slug } = useParams();
+  const [loading, setLoading] = useState(false);
+
   // state
   const [product, setProduct] = useState(null);
+  
+  const navigate = useNavigate();
+
 
   useEffect(() => {
     loadProducts();
   }, []);
+
 
   const loadProducts = async () => {
     try {
@@ -26,31 +35,42 @@ const AdminProductDetail = () => {
       console.log(err);
     }
   };
+  
+  const handleDelete = async () => {
+    setLoading(true)
+    try {
+      const { data } = await axios.delete(`/product/${product?._id}`);
+      if (!data?.error) {
+        setLoading(false);
+        toast.success(`"${product?.name}" is deleted`);
+        setShow(false);
+      }
+    } catch (err) {
+      console.log(err);
+      const msg = err?.response?.data?.error;
+      toast.error(msg);
+      setLoading(false);
+      setTimeout(() => {
+        navigate("/dashboard/admin-products");
+      }, 5000);
+    }
+  };
 
   console.log(product);
 
   return (
     <>
-    <Menu/>
+      <Menu />
       <Jumbotron
         title={`Hello ${auth?.user?.name}`}
         subTitle="Admin Dashboard"
       />
 
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-9">
-            <div className="p-3 mt-2 mb-2 h4 bg-light">Product</div>
-            {/* AdminProductDetail card here */}
-            {/* Features in this AdminProductDetail page include delete, link to update, etc */}
-
-            {product?.name}
-
-            <Link to={`/dashboard/admin-product/update/${slug}`}>Update</Link>
-          </div>
-        </div>
+      <div className="container">
+        <AdminProductCard product={product} slug={slug} handleDelete={handleDelete} loading={loading} />
+        {/* Features in this AdminProductDetail page include delete, link to update, etc */}
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 };
